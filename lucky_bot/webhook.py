365323ebd7285a0ef0bf46f9dd36cc88b83df0ea
voronokKita <kitavoronok@gmail.com>
2022-12-26 14:17:29 +0300

    webhook thread

--- lucky_bot/webhook.py
+++ lucky_bot/webhook.py
@@ -1,4 +1,36 @@
-from lucky_bot.helpers.signals import WEBHOOK_IS_RUNNING
+import threading
 
-def webhook_thread():
-    WEBHOOK_IS_RUNNING.set()
+from lucky_bot.helpers.signals import WEBHOOK_IS_RUNNING, EXIT_SIGNAL
+
+
+class WebhookThread(threading.Thread):
+    exception = None
+
+    def __str__(self):
+        return 'webhook thread'
+
+    def __int__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        try:
+            # TODO
+
+            WEBHOOK_IS_RUNNING.set()
+
+            self._test_exception()
+
+            if EXIT_SIGNAL.wait():
+                pass
+
+        except Exception as e:
+            self.exception = e
+
+    def stop(self):
+        threading.Thread.join(self, 1)
+        if self.exception:
+            raise self.exception
+
+    @staticmethod
+    def _test_exception():
+        pass
--- tests/units/test_webhook.py
+++ tests/units/test_webhook.py
@@ -1,1 +1,45 @@
- 
+""" python -m unittest tests.units.test_webhook """
+import unittest
+from unittest.mock import patch
+from time import sleep
+
+from lucky_bot.helpers.constants import TestException
+from lucky_bot.helpers.signals import WEBHOOK_IS_RUNNING, EXIT_SIGNAL
+from lucky_bot.webhook import WebhookThread
+
+
+class WebhookTestException(Exception):
+    ...
+
+
+class TestWebhook(unittest.TestCase):
+    def setUp(self):
+        self.webhook = WebhookThread()
+
+    def tearDown(self):
+        if self.webhook.is_alive():
+            EXIT_SIGNAL.set()
+            self.webhook.join(3)
+
+    def test_threading(self):
+        self.webhook.start()
+
+        if WEBHOOK_IS_RUNNING.wait(10):
+            EXIT_SIGNAL.set()
+            sleep(0.1)
+            self.webhook.stop()
+            self.assertFalse(self.webhook.is_alive())
+
+        else:
+            raise WebhookTestException('The time to start the webhook has passed.')
+
+    @patch('lucky_bot.webhook.WebhookThread._test_exception')
+    def test_threading_exception(self, mock_exception):
+        mock_exception.side_effect = TestException('boom')
+        self.webhook.start()
+
+        if WEBHOOK_IS_RUNNING.wait(10):
+            self.assertRaises(TestException, self.webhook.stop)
+            self.assertFalse(self.webhook.is_alive())
+        else:
+            raise WebhookTestException('The time to start the webhook has passed.')
