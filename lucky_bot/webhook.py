0ca11e36e85d32310b6723eff5dbb3638df42c4a
voronokKita <kitavoronok@gmail.com>
2022-12-29 12:04:58 +0300

    pyngrok

--- lucky_bot/webhook.py
+++ lucky_bot/webhook.py
@@ -1,5 +1,9 @@
+from pyngrok import ngrok
+from pyngrok.exception import PyngrokNgrokURLError
+
 from lucky_bot.helpers.signals import WEBHOOK_IS_RUNNING, WEBHOOK_IS_STOPPED, EXIT_SIGNAL
 from lucky_bot.helpers.misc import ThreadTemplate
+from lucky_bot.helpers.constants import REPLIT, REPLIT_URL, WEBHOOK_ENDPOINT, PORT, TESTING
 
 import logging
 from logs.config import console, event
@@ -10,11 +14,33 @@ class WebhookThread(ThreadTemplate):
     is_running_signal = WEBHOOK_IS_RUNNING
     is_stopped_signal = WEBHOOK_IS_STOPPED
 
+    tunnel = None
+    webhook_url = None
+
     def __str__(self):
         return 'webhook thread'
 
     def body(self):
+        self._make_tunnel()
+
         self._set_the_signal()
         self._test_exception()
         if EXIT_SIGNAL.wait():
             pass
+
+        self._close_tunnel()
+
+    def _make_tunnel(self):
+        if REPLIT:
+            self.webhook_url = REPLIT_URL + WEBHOOK_ENDPOINT
+        else:
+            self.tunnel = ngrok.connect(PORT, proto='http', bind_tls=True)
+            console(self.tunnel)
+            self.webhook_url = self.tunnel.public_url + WEBHOOK_ENDPOINT
+
+    def _close_tunnel(self):
+        try:
+            ngrok.disconnect(self.tunnel.public_url)
+        except (PyngrokNgrokURLError, Exception):
+            pass
+        ngrok.kill()
--- tests/units/test_webhook.py
+++ tests/units/test_webhook.py
@@ -1,11 +1,13 @@
 """ python -m unittest tests.units.test_webhook """
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from lucky_bot.webhook import WebhookThread
 from lucky_bot.helpers.signals import WEBHOOK_IS_RUNNING, WEBHOOK_IS_STOPPED
+from lucky_bot.helpers.constants import TestException, REPLIT, PORT, WEBHOOK_ENDPOINT
 
 from tests.presets import ThreadTestTemplate
 
+# TODO mock normal start
 
 class TestWebhookBase(ThreadTestTemplate):
     thread_class = WebhookThread
@@ -18,3 +20,20 @@ class TestWebhookBase(ThreadTestTemplate):
     @patch('lucky_bot.helpers.misc.ThreadTemplate._test_exception')
     def test_webhook_exception_case(self, test_exception):
         super().exception_case(test_exception)
+
+    @patch('lucky_bot.webhook.ngrok')
+    def test_webhook_tunnel(self, ngrok):
+        self.assertFalse(REPLIT)
+
+        tunnel = Mock()
+        tunnel.public_url = 'http://example.com'
+        ngrok.connect.return_value = tunnel
+
+        self.thread_obj._make_tunnel()
+        self.thread_obj._close_tunnel()
+
+        ngrok.connect.assert_called_once_with(PORT, proto='http', bind_tls=True)
+        self.assertEqual(self.thread_obj.webhook_url, 'http://example.com' + WEBHOOK_ENDPOINT)
+        ngrok.disconnect.assert_called_once_with('http://example.com')
+        ngrok.kill.assert_called_once()
+
