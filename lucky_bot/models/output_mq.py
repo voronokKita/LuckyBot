86ed753647b3a8ec1c7340959c0c9210b610b2b9
voronokKita <kitavoronok@gmail.com>
2023-01-04 16:10:35 +0300

    output message queue

--- lucky_bot/models/output_mq.py
+++ lucky_bot/models/output_mq.py
@@ -1,13 +1,9 @@
-from sqlalchemy import (
-    create_engine,
-    Column, Integer, Text,
-)
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, Text, BLOB
+from sqlalchemy.orm import declarative_base, sessionmaker, Query
 
-from lucky_bot.helpers.constants import OUTPUT_MQ_FILE
+from lucky_bot.helpers.constants import TESTING, OUTPUT_MQ_FILE
 
 import logging
-from logs.config import console, event
 logger = logging.getLogger(__name__)
 
 OMQ_ENGINE = create_engine(f'sqlite:///{OUTPUT_MQ_FILE}', future=True)
@@ -17,12 +13,47 @@ OMQ_SESSION = sessionmaker(bind=OMQ_ENGINE)
 OMQBase = declarative_base()
 
 
-class OutputMessage(OMQBase):
+class OutgoingMessage(OMQBase):
     __tablename__ = 'messages_to_telegram'
 
     id = Column(Integer, primary_key=True)
-    data = Column(Text, nullable=False)
+    text = Column('message_text', Text, nullable=False)
+    stream = Column('message_file', BLOB, nullable=True)
     time = Column('message_date', Integer, nullable=False)
 
     def __str__(self):
         return f'<outgoing message id-{self.id!r}>'
+
+
+class OutputQueue:
+    @staticmethod
+    def set_up():
+        OMQBase.metadata.create_all(OMQ_ENGINE)
+
+    @staticmethod
+    def tear_down():
+        if TESTING and OUTPUT_MQ_FILE.exists():
+            OMQBase.metadata.drop_all(OMQ_ENGINE)
+
+    @staticmethod
+    def add_message(message, date, file=None):
+        msg_obj = OutgoingMessage(text=message, time=date)
+        with OMQ_SESSION.begin() as session:
+            session.add(msg_obj)
+
+    @staticmethod
+    def get_first_message() -> Query | None:
+        ''' FIFO '''
+        with OMQ_SESSION() as session:
+            msg_obj = session.query(OutgoingMessage)\
+                .order_by(OutgoingMessage.time).first()
+            return msg_obj
+
+    @staticmethod
+    def delete_message(msg_obj):
+        with OMQ_SESSION.begin() as session:
+            session.delete(msg_obj)
+
+
+if not TESTING and not OUTPUT_MQ_FILE.exists():
+    OutputQueue.set_up()
--- tests/units/test_sender.py
+++ tests/units/test_sender.py
@@ -1,8 +1,12 @@
 """ python -m unittest tests.units.test_sender """
+import unittest
 from unittest.mock import patch
 
 from lucky_bot.sender import SenderThread
-from lucky_bot.helpers.signals import SENDER_IS_RUNNING, SENDER_IS_STOPPED
+from lucky_bot.helpers.signals import (
+    SENDER_IS_RUNNING, SENDER_IS_STOPPED, EXIT_SIGNAL,
+)
+from lucky_bot.models.output_mq import OutputQueue
 
 from tests.presets import ThreadTestTemplate
 
@@ -18,3 +22,40 @@ class TestSenderBase(ThreadTestTemplate):
     @patch('lucky_bot.helpers.misc.ThreadTemplate._test_exception')
     def test_sender_exception_case(self, test_exception):
         super().exception_case(test_exception)
+
+    def sender_exception_after_message_signal(self):
+        pass
+
+    def sender_message(self):
+        pass
+
+
+class SenderMessageQueue(unittest.TestCase):
+    def setUp(self):
+        OutputQueue.set_up()
+
+    def tearDown(self):
+        OutputQueue.tear_down()
+
+    def test_output_queue_works(self):
+        OutputQueue.add_message('foo', 1)
+        OutputQueue.add_message('bar', 2)
+        OutputQueue.add_message('baz', 3)
+
+        msg_obj = OutputQueue.get_first_message()
+        self.assertIsNotNone(msg_obj, msg='foo')
+        self.assertEqual(msg_obj.text, 'foo')
+        OutputQueue.delete_message(msg_obj)
+
+        msg_obj = OutputQueue.get_first_message()
+        self.assertIsNotNone(msg_obj, msg='bar')
+        self.assertEqual(msg_obj.text, 'bar')
+        OutputQueue.delete_message(msg_obj)
+
+        msg_obj = OutputQueue.get_first_message()
+        self.assertIsNotNone(msg_obj, msg='baz')
+        self.assertEqual(msg_obj.text, 'baz')
+        OutputQueue.delete_message(msg_obj)
+
+        result = OutputQueue.get_first_message()
+        self.assertIsNone(result)
